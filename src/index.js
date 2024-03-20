98eb45b1f9a55d66bc4e0a62f1e8de3a3253a8c7
Marta <marta.af.lab@gmail.com>
2024-03-20 21:35:16 +0100

    add endpoint list recipes by id and capture errors

--- src/index.js
+++ src/index.js
@@ -13,19 +13,19 @@ server.use(cors());
 server.use(express.json({ limit: '25mb' }));
 
 //Configurar MSQL
-async function getConnection()  {
+async function getConnection() {
     const connection = await mysql.createConnection({
-        host: process.env.MYSQL_HOST  || 'localhost',
+        host: process.env.MYSQL_HOST || 'localhost',
         user: process.env.MYSQL_USER || 'root',
         password: process.env.MYSQL_PASS,
-        database:'recetas_db'
+        database: 'recetas_db'
     });
     await connection.connect();
 
     console.log(
-      `Conexión establecida con la base de datos (identificador=${connection.threadId})`
+        `Conexión establecida con la base de datos (identificador=${connection.threadId})`
     );
-  
+
     return connection;
 }
 
@@ -37,16 +37,57 @@ server.listen(port, () => {
 
 //EndPoint
 //Listar recetas
-server.get('/api/recetas', async (req, res) =>  {
-
+server.get('/api/recetas', async (req, res) => {
+try {
     const conn = await getConnection();
-    const queryGetRecetas = 
-    `SELECT * FROM recetas;
+    const queryGetRecetas =
+        `SELECT * FROM recetas;
     `;
- const [results] = await conn.query( queryGetRecetas);
- res.json ({
-    info: {count: results.length},
-    result: results,
-})
-  
+    const [results] = await conn.query(queryGetRecetas);
+    conn.end();
+    res.json({
+        info: { count: results.length },
+        result: results,
+    });
+}
+catch (error) {
+    console.error("Error al obtener el listado de recetas:", error);
+    res.status(500).json({ error: "Error en la Base de datos" });
+}
+
+});
+
+//Obtener receta por id
+server.get('/api/recetas/:id', async (req, res) => {
+    const recetaId = req.params.id; // Obtener por query params el id de la receta
+
+    try {
+        const conn = await getConnection();
+        let queryGetId = `
+        SELECT * 
+        FROM recetas
+        WHERE id = ?;`;
+
+        const [results] = await conn.query(queryGetId, [recetaId]);
+
+        if (results.length === 0) {
+            return res.status(404).json({ error: "Receta no encontrada" });
+        }
+        const receta = results[0];
+
+        res.json({
+            results: {
+           
+                name: receta.nombre,
+                ingredientes: receta.ingredientes,
+                instrucciones: receta.instrucciones
+           
+        }
+});
+conn.end();
+    
+  }  catch (error) {
+    console.error("Error al obtener la receta:", error);
+    res.status(500).json({ error: "Error en la Base de datos" });
+}
 });
