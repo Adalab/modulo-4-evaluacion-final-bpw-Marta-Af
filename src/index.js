f76e865aec6dfb241bde5ebd13c9c522cd69ed58
Marta <marta.af.lab@gmail.com>
2024-03-20 19:40:10 +0100

    create api and endpoint list recipes

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const mysql = require('mysql2/promise');
 
 //Crear Variables
 const server = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 //Configurar expres
 server.use(cors());
@@ -14,13 +14,11 @@ server.use(express.json({ limit: '25mb' }));
 
 //Configurar MSQL
 async function getConnection()  {
-
-   
     const connection = await mysql.createConnection({
         host: process.env.MYSQL_HOST  || 'localhost',
         user: process.env.MYSQL_USER || 'root',
         password: process.env.MYSQL_PASS,
-        database: process.env.MYSQL_SCHEMA || 'recetas_db'
+        database:'recetas_db'
     });
     await connection.connect();
 
@@ -35,7 +33,20 @@ async function getConnection()  {
 //Arrancar servidor
 server.listen(port, () => {
     console.log(`Server has been started in http://localhost:${port}`)
-})
+});
 
 //EndPoint
-getConnection();
+//Listar recetas
+server.get('/api/recetas', async (req, res) =>  {
+
+    const conn = await getConnection();
+    const queryGetRecetas = 
+    `SELECT * FROM recetas;
+    `;
+ const [results] = await conn.query( queryGetRecetas);
+ res.json ({
+    info: {count: results.length},
+    result: results,
+})
+  
+});
